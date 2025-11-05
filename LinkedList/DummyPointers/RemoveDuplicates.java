289ce6849d800a859932010732d7869c4e671992
KingmakerKou <160477394+KingmakerKou@users.noreply.github.com>
2025-11-05 23:53:02 +0530

    Update RemoveDuplicates.java

--- LinkedList/DummyPointers/RemoveDuplicates.java
+++ LinkedList/DummyPointers/RemoveDuplicates.java
@@ -1,3 +1,5 @@
+// Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
