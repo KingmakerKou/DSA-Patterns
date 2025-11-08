072dbf263e2a547caa39bbebea4eacd1b1e9321a
KingmakerKou <160477394+KingmakerKou@users.noreply.github.com>
2025-11-08 10:17:04 +0530

    Update GroupAnagram.java

--- Strings/TwoPointers/GroupAnagram.java
+++ Strings/TwoPointers/GroupAnagram.java
@@ -22,3 +22,44 @@ class Solution {
 }
 
 //optmised
+    // find the frequency of each string instead of sorting them
+    //time -> O(k . n)
+    //space -> O(k . n)
+class Solution {
+    public String FindFreq(String str){
+        int[] hash = new int[26];
+        for(char ch : str.toCharArray()){
+            hash[ch - 'a']++;
+        }
+
+        StringBuilder strr = new StringBuilder();
+        char c = 'a';
+        for(int i : hash){
+            strr.append(c);
+            strr.append(i);
+            c++;
+        }
+    return strr.toString();
+    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs.length == 0){
+            return new ArrayList<>();
+        }
+        int n = strs.length;
+        Map<String , List<String>> mpp = new HashMap<>();
+
+        for(int i=0;i<n;i++){
+            String freq = FindFreq(strs[i]);
+
+            if(mpp.containsKey(freq)){
+                mpp.get(freq).add(strs[i]);
+            }else{
+                List<String> temp = new ArrayList<>();
+                temp.add(strs[i]);
+                mpp.put(freq , temp);
+            }
+           
+        }
+    return new ArrayList<>(mpp.values());
+    }
+}
